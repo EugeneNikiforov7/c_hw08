13568b7c52bcb7308c900a72eedac985d508caa6
EugeneNikiforov <eugene_vn@mail.ru>
2023-08-01 23:08:55 +0500

    edit task 58

--- task3/Program.cs
+++ task3/Program.cs
@@ -6,55 +6,66 @@
 // 18 20
 // 15 18
 
-int[,] MultipleArray(int[,] arr1, int[] arr2)
+int[,] MultipleArray(int[,] arr1, int[,] arr2)
 {
+    int maxLegth = 0;
     if (arr1.GetLength(0) > arr1.GetLength(1))
-        int[,] multArr = new int[arr1.GetLength(0), arr1.GetLength(0)];
-    else
-        int[,] multArr = new int[arr1.GetLength(1), arr1.GetLength(1)];
-    for (int i = 0; i < arr1.GetLength(0); i++)
     {
-        for (int j = 0; j < arr2.GetLength(1); j++)
+        maxLegth = arr1.GetLength(0);
+    }
+    else
+    {
+        maxLegth = arr1.GetLength(1);
+    }
+    int[,] multArr = new int[maxLegth, maxLegth];
+
+    for (int k = 0; k < arr1.GetLength(0); k++)
+    {
+        for (int i = 0; i < arr1.GetLength(1); i++)
         {
-            multArr[i, j] += arr1[i, j];
+            for (int j = 0; j < arr2.GetLength(1); j++)
+            {
+                //multArr[k, i] += arr1[i, j] * arr2[j, k];
+                multArr[i, k] += arr1[i, j] * arr2[j, k];
+            }
         }
-        //        Console.Write($"{sumRows[i]} ");
     }
     return multArr;
+}
 
-    void PrintDoubleArray(int[,] arr)
+void PrintDoubleArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int i = 0; i < arr.GetLength(0); i++)
-        {
-            for (int j = 0; j < arr.GetLength(1); j++)
-                Console.Write($"{arr[i, j]} ");
-            Console.WriteLine();
-        }
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j].ToString().PadLeft(4));
         Console.WriteLine();
     }
+    Console.WriteLine();
+}
 
-    int[,] CreateRandomArray(int row, int col)
-    {
-        int[,] arr = new int[row, col];
-        for (int i = 0; i < arr.GetLength(0); i++)
-            for (int j = 0; j < arr.GetLength(1); j++)
-                arr[i, j] = new Random().Next(1, 10);
-        return arr;
-    }
+int[,] CreateRandomArray(int row, int col)
+{
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = new Random().Next(1, 10);
+    return arr;
+}
 
-    int GetNum(string message)
-    {
-        Console.Write(message);
-        return Convert.ToInt32(Console.ReadLine());
-    }
+int GetNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-    int num1 = GetNum("Введите количество строк первого массива: ");
-    int num2 = GetNum("Введите количество колонок первого массива: ");
-    int[,] doubleArray1 = CreateRandomArray(num1, num2);
-    int[,] doubleArray2 = CreateRandomArray(num2, num1);
-    PrintDoubleArray(doubleArray1);
-    PrintDoubleArray(doubleArray1);
-    Console.WriteLine("Результирующая матрица:");
-    int[,] doubleArray3 = MultipleArray(doubleArray1, doubleArray2);
-    PrintDoubleArray(doubleArray3);
+int num1 = GetNum("Введите количество строк первого массива: ");
+int num2 = GetNum("Введите количество колонок первого массива: ");
+int[,] doubleArray1 = CreateRandomArray(num1, num2);
+int[,] doubleArray2 = CreateRandomArray(num2, num1);
+PrintDoubleArray(doubleArray1);
+PrintDoubleArray(doubleArray2);
+Console.WriteLine("Результирующая матрица:");
+int[,] doubleArray3 = MultipleArray(doubleArray1, doubleArray2);
+PrintDoubleArray(doubleArray3);
 
