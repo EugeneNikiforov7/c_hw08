f4e01ce5b30d4398157ed024da562179a2798aab
EugeneNikiforov <eugene_vn@mail.ru>
2023-08-03 15:52:53 +0500

    62

--- task5/Program.cs
+++ task5/Program.cs
@@ -4,3 +4,39 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+void FillArray(int[,] arr)
+{
+    int f = 1;
+    int n = 0;
+    while (n < arr.GetLength(0) / 2)
+    {
+    for (int i = 0 + n; i < arr.GetLength(0) - n; i++) arr[n, i] = f++;
+    for (int i = 1 + n; i < arr.GetLength(1) - n; i++) arr[i, arr.GetLength(1) - 1 - n] = f++;
+    for (int i = n + 1; i < arr.GetLength(0) - n; i++) arr[arr.GetLength(1) - 1 - n, arr.GetLength(1) - 1 - i] = f++;
+    for (int i = n + 1; i < arr.GetLength(0) - n - 1; i++) arr[arr.GetLength(1) - 1 - i, n] = f++;
+    n++;
+    }
+    if (arr.GetLength(0) % 2 != 0) arr[arr.GetLength(0) / 2, arr.GetLength(1) / 2] = arr.GetLength(0) * arr.GetLength(1);
+}
+
+void PrintDoubleArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j].ToString().PadLeft(4));
+            Console.WriteLine();
+    }
+}
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetNum("Введите размер массива: ");
+int[,] array = new int[num, num];
+FillArray(array);
+PrintDoubleArray(array);
